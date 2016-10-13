7ab99406d90545614612131f71d500b26a860656
isain <isain_5@hotmail.com>
2016-10-13 16:16:59 -0500

    Initial commit

---
+++ desktop/src/mx/izo/xportal/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package mx.izo.xportal.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import mx.izo.xportal.Plataforma;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Plataforma(), config);
+	}
+}
